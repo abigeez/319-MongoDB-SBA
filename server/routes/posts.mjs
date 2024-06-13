7d0e3b9b0b42d619608ee85f6e2bc369df979bdd
Abi Garner <abigail.gee@hotmail.com>
2024-06-13 01:14:06 -0400

    pipeline

--- server/routes/posts.mjs
+++ server/routes/posts.mjs
@@ -2,7 +2,8 @@
 router.get("/latest", async (req,res) =>{
     let collection = await db.collection("posts");
     let results = await collection.aggregate([
-        
+        {"$project": {"author":1, "title":1, "tags":1, "date":1}},
+    {"$sort": {"date: -1"}},
     ])
     .limit(20)
     .toArray();
