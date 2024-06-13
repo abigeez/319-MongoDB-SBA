929fd752c64e17ed4e29d6fd4ad20bd1a466f41d
Abi Garner <abigail.gee@hotmail.com>
2024-06-13 01:11:40 -0400

    adding aggs

--- server/routes/posts.mjs
+++ server/routes/posts.mjs
@@ -1,7 +1,9 @@
 
-router.get("/", async (req,res) =>{
+router.get("/latest", async (req,res) =>{
     let collection = await db.collection("posts");
-    let results = await collection.find({})
+    let results = await collection.aggregate([
+        
+    ])
     .limit(20)
     .toArray();
     res.send(results).status(200);
